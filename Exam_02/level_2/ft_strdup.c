59b4e7b42f6006c2597ddc8be0cd2b538e99c7c3
Diana <dianabali64@gmail.com>
2026-07-24 13:17:13 +0200

    Implement ft_strdup
    Implement ft_strdup function to duplicate a string.

---
+++ Exam_02/level_2/ft_strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+// #include <stdio.h>
+
+int ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char *ft_strdup(char *src)
+{
+	char *new = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (!new)
+		return (NULL);
+	int i = 0;
+	while (src[i])
+	{
+		new[i] = src[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+/* int main(void)
+{
+	char s[] = "hello";
+	printf("%s", ft_strdup(s));
+} */
