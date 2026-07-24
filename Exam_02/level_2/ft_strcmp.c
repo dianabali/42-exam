94541bad52340e4672e05864e69c5561b932e4fb
Diana <dianabali64@gmail.com>
2026-07-24 12:07:25 +0200

    Implement ft_strcmp

---
+++ Exam_02/level_2/ft_strcmp.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+
+int ft_strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+/* int main(void)
+{
+	char *s1 = "2";
+	char *s2 = "2";
+	printf("%d", ft_strcmp(s1, s2));
+} */
