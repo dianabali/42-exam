e16569d7a35c07717a0dbbc01442e7a2efd57ad4
Diana <dianabali64@gmail.com>
2026-07-24 13:48:02 +0200

    Implement ft_strrev

---
+++ Exam_02/level_2/ft_strrev.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+char *ft_strrev(char *str)
+{
+    int start = 0;
+    int end = 0;
+    char temp;
+
+    while (str[end])
+        end++;
+    end--;
+
+    while (start < end)
+    {
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+    return (str);
+}
+
+int main(void)
+{
+	char str[] = "hello";
+	printf("%s", ft_strrev(str));
+}
