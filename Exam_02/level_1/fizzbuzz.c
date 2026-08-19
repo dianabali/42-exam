d40bb05cfb6bee6c8f4bce0d6d6a9a6a83004b6f
Diana <dianabali64@gmail.com>
2026-08-19 15:49:14 +0200

    Fix fizzbuzz logic

--- Exam_02/level_1/fizzbuzz.c
+++ Exam_02/level_1/fizzbuzz.c
@@ -22,12 +22,12 @@ int main(void)
     int i = 1;
     while (i <= 100)
     {
+        if (i % 3 == 0 && i % 5 == 0)
+            write(1, "fizzbuzz\n", 9);
         if (i % 3 == 0)
             write(1, "fizz", 4);
         else if (i % 5 == 0)
             write(1, "buzz", 4);
-        else if (i % 3 == 0 && i % 5 == 0)
-            write(1, "fizzbuzz", 8);
         else
             ft_putnbr(i);
         write(1, "\n", 1);
